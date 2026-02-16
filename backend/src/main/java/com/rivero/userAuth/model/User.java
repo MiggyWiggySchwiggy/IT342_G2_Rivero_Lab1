c677ebd26e929b3f426918781f3ddab860ec031e
MiggyWiggySchwiggy <mmcrivero123@gmail.com>
2026-02-16 10:11:16 +0800

    Fixing Login

--- backend/src/main/java/com/rivero/userAuth/model/User.java
+++ backend/src/main/java/com/rivero/userAuth/model/User.java
@@ -1,7 +1,7 @@
 package com.rivero.userAuth.model;
 
 import jakarta.persistence.*;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty; // Import this!
 
 @Entity
 @Table(name = "users")
@@ -24,10 +24,11 @@ public class User {
     private String email;
 
     @Column(nullable = false)
-    @JsonIgnore
+    // CHANGE IS HERE: Use WRITE_ONLY instead of @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String passwordHash;
 
-    // --- MANUAL CONSTRUCTORS ---
+    // --- CONSTRUCTORS ---
     public User() {}
 
     public User(String username, String firstName, String lastName, String email, String passwordHash) {
@@ -38,52 +39,22 @@ public class User {
         this.passwordHash = passwordHash;
     }
 
-    // --- MANUAL GETTERS AND SETTERS ---
-    public Long getUserID() {
-        return userID;
-    }
+    // --- GETTERS AND SETTERS ---
+    public Long getUserID() { return userID; }
+    public void setUserID(Long userID) { this.userID = userID; }
 
-    public void setUserID(Long userID) {
-        this.userID = userID;
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getUsername() {
-        return username;
-    }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
 }
